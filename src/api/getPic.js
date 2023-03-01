cf7b62d2b1ea48d8537f271a007ab31557f9b3e6
huweiye <1045922393@qq.com>
2023-03-01 15:47:25 +0800

    random pic

--- src/api/getPic.js
+++ src/api/getPic.js
@@ -1,37 +1,51 @@
-import axios from 'axios'
+import axios from "axios";
 
 let time = 0;
 
 const apiVal = {
   categories: {
-    '100': '普通',
-    '010': '动漫',
-    '001': '人物'
+    100: "普通",
+    "010": "动漫",
+    "001": "人物",
   },
   purity: {
-    '100': '正常',
-    '010': '开放',
-    '001': '贤者'
+    100: "正常",
+    "010": "开放",
+    "001": "贤者",
   },
   sorting: {
-    'date-added': '新增',
-    'random': '随机',
-    'views': '浏览',
-    'favorites': '收藏',
-    'toplist': '置顶'
+    "date-added": "新增",
+    random: "随机",
+    views: "浏览",
+    favorites: "收藏",
+    toplist: "置顶",
   },
   atleast: {
-    '1080p': '1920x1080',
-    '2k': '2560x1440',
-    '4k': '3840x2160'
-  }
-}
-const isDev = process.env.NODE_ENV === 'development' // development|production
-const baseUrl = isDev ? '/wallhaven' : 'https://wallhaven.cc'
-export const getPic = async (purity = '010') => {
-  console.log(purity, 'api purity');
-  let api = baseUrl + `/api/v1/search?purity=${purity}&categories=100&atleast=3840x2160`;
-  const res = await axios.get(api)
+    "1080p": "1920x1080",
+    "2k": "2560x1440",
+    "4k": "3840x2160",
+  },
+};
+const isDev = process.env.NODE_ENV === "development"; // development|production
+const baseUrl = isDev ? "/wallhaven" : "https://wallhaven.cc";
+export const getPic = async (
+  purity = "010",
+  options = {
+    categories: "100",
+    sorting: "random",
+    atleast: apiVal.atleast["4k"],
+  },
+) => {
+  // 默认值
+  const {
+    categories = "100",
+    sorting = "random",
+    atleast = apiVal.atleast["4k"],
+  } = options;
+  let api =
+    baseUrl +
+    `/api/v1/search?purity=${purity}&categories=${categories}&sorting=${sorting}&atleast=${atleast}`;
+  const res = await axios.get(api);
   console.log(`>>>>>>getPic|${api}| request times is>>>>>`, ++time);
-  return res.data.data
-}
+  return res.data.data;
+};
---
+++ src/pages/picture.js
@@ -0,0 +1,104 @@
+import "./picture.css";
+import { getPic } from "../api/getPic";
+import { getPicMock } from "../mock/getPic";
+import React from "react";
+/**
+ * desc: 自动换图片
+ * 双击可以换图片
+ */ 
+
+class Pic extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      picList: [],
+      curImg: {},
+      imgIndex: (Math.random() * 1000) | 0,
+      loading: true,
+    };
+    this.timeId = null
+  }
+  async freshImgList() {
+    let res = getPicMock.data;
+    this.setState({
+      picList: res,
+      curImg: res[0],
+    });
+    res = await getPic(this.props.purity || '010');
+    this.timeId = setTimeout(()=>{
+      clearTimeout(this.timeId);
+      this.setState({
+        picList: res,
+        curImg: res[0],
+      });
+    },25000)
+    
+  }
+
+  nextImg() {
+    this.setState({
+      imgIndex: this.state.imgIndex + 1,
+    });
+  }
+
+  loadedImg() {
+    this.setState({
+      loading: false,
+    });
+  }
+  loadingImg() {
+    this.setState({
+      loading: true,
+    });
+  }
+
+  async componentDidMount() {
+    console.log(this.props)
+    this.freshImgList();
+  }
+  imgError = () => {
+    this.loadingImg();
+    this.nextImg();
+  };
+  imgLoad = () => {
+    this.loadedImg();
+    this.timeId = setTimeout(() => {
+      clearTimeout(this.timeId);
+      this.nextImg();
+    }, 25000);
+  };
+  imgClick =()=>{
+    this.imgError();
+  }
+  render() {
+    const { imgIndex, picList, loading } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          {picList.length !== 0 ? (
+            <img
+              className={loading ? "img_hide" : ""}
+              src={picList[imgIndex % picList.length].path}
+              onError={this.imgError.bind(this)}
+              onLoad={this.imgLoad}
+              onDoubleClick={this.imgClick}
+              alt="lose it"
+            />
+          ) : (
+            "isLoading"
+          )}
+          {loading ? (
+            <>
+              <div className="loading"></div>
+              <div className="loading"></div>
+            </>
+          ) : (
+            ""
+          )}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Pic;

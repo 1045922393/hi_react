b08f80e42f38b6676641b18ad194821f10ac1cbc
huweiye <1045922393@qq.com>
2023-03-03 11:42:47 +0800

    加入目录

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Picture from './pages/picture';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+// import Picture from "@/pages/picture";
+// import Pho from "@/pages/photos";
+// import Content from '@/pages/content/index';
+// 优化
+const Picture = React.lazy(()=>import("@/pages/picture"));
+const Pho = React.lazy(()=>import("@/pages/photos"));
+const Content = React.lazy(()=>import("@/pages/content/index"));
+
+ const config = {
+  homepage: "/picture",
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-    <Picture />
-  );
+  <BrowserRouter>
+    <Routes>
+      <Route path={config.homepage + "/"} element={<Content />}></Route>
+      <Route path={config.homepage + "/photo"} element={<Pho />}></Route>
+      <Route path={config.homepage + "/picture"} element={<Picture />}></Route>
+      <Route path={config.homepage + "/*"} element={<Picture />}></Route>
+    </Routes>
+  </BrowserRouter>,
+);
+
+export {config}
---
+++ src/pages/photos.js
@@ -0,0 +1,14 @@
+import "@/pages/photos.css";
+import React from "react";
+
+function Pho() {
+  return (
+    <>
+      <img src="https://api.isoyu.com/mm_images.php" alt="" />
+      <img src="http://api.btstu.cn/sjbz/?lx=m_meizi" alt="" />
+      <img src="https://cdn.seovx.com/?mom=302" alt="" />
+    </>
+  );
+}
+
+export default Pho;
